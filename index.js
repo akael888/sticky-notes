c03c3194555ce796b1b6209e641029cb744224c1
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-06-13 18:13:26 +0900

    feat : Added Ways to Add Sticky Notes through

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const defaultNotes = [
   { notesID: 0, notesMsg: "ABC", notesTimeStamp: 1776085680 },
+  { notesID: 1, notesMsg: "yes", notesTimeStamp: 1776085680 },
+  { notesID: 2, noteMsg: "Test 1234", notesTimeStamp: 1776085680 },
 ];
 
-const defaultElement = document.getElementById(
-  `note-${defaultNotes[0].notesID}`,
-);
+const mainBoard = document.getElementById("main-board");
+
+defaultNotes.map((e) => {
+  const newDiv = document.createElement("div");
+  newDiv.className = "sticky-notes";
+  newDiv.textContent = e.notesMsg;
+  mainBoard.append(newDiv);
+});
+
+const newDiv = document.createElement("div");
 
 defaultElement.textContent = defaultNotes[0].notesMsg;
